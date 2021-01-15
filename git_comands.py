bccfecb969536a8f294f8c3525c9270943724f74
Adis <adis_98.kg@mail.ru>
2021-01-15 11:09:25 +0600

    initial commit.

---
+++ git_comands.py
@@ -0,0 +1,11 @@
+git init -.git
+git remote add name url
+git pull origin master
+git status
+git add
+git commit -m 'comment'
+git branch
+git branch name branche
+git checkout name branch
+got push origin master
+git reset filename
